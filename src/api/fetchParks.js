accd229484c32b146cea8d4e773dc65977114c66
Eric Vuu <ericlv91@gmail.com>
2024-10-28 15:52:21 -0700

    improved query by activity to render consistemt count

--- src/api/fetchParks.js
+++ src/api/fetchParks.js
@@ -1,28 +1,45 @@
 import axios from "axios";
 import notFoundImage from "../assets/images/banners/not-found.jpg";
 
-export const fetchParks = async ({ queryKey, pageParam}) => {
+export const fetchParks = async ({ queryKey, pageParam }) => {
   const [_, qStateCode, qSearchTerm, parksPerPage, formattedActivity] = queryKey;
   const npsAPIKeys = import.meta.env.VITE_NPS_API_Keys;
-  const startCount = pageParam || 0;
+  let startCount = pageParam || 0;
+  let parksData = [];
+  let totalFetched = 0;
+  let totalParks = 0;
 
-  const apiUrl = `https://developer.nps.gov/api/v1/parks?api_key=${npsAPIKeys}${qStateCode ? `&stateCode=${qStateCode}` : ""}${qSearchTerm ? `&q=${qSearchTerm}` : ""}${startCount ? `&start=${startCount}` : ""}${parksPerPage ? `&limit=${parksPerPage}` : ""}`;
+  while (parksData.length < parksPerPage) {
+    const apiUrl = `https://developer.nps.gov/api/v1/parks?api_key=${npsAPIKeys}${qStateCode ? `&stateCode=${qStateCode}` : ""}${qSearchTerm ? `&q=${qSearchTerm}` : ""}&start=${startCount}&limit=${parksPerPage}`;
+    const res = await axios.get(apiUrl);
 
-  const res = await axios.get(apiUrl);
+    if (res.status !== 200) {
+      throw new Error("Failed to fetch parks");
+    }
 
-  if (res.status !== 200) {
-    throw new Error("Failed to fetch parks");
+    const fetchedParksBatch = res.data.data;
+    totalParks = res.data.total;
+
+    const filteredParks = formattedActivity
+      ? fetchedParksBatch.filter(park =>
+          park.activities.some(
+            act => act.name.toLowerCase() === formattedActivity.toLowerCase()
+          )
+        )
+      : fetchedParksBatch;
+
+    parksData = parksData.concat(filteredParks);
+    totalFetched += fetchedParksBatch.length;
+
+    startCount += fetchedParksBatch.length;
+
+    if (fetchedParksBatch.length < parksPerPage) break;
   }
 
-  let parksData = res.data.data;
-  let parksBatchCount = parksData.length;
-
-  if (formattedActivity && typeof formattedActivity === "string") {
-    parksData = parksData.filter((park) =>
-      park.activities.some(
-        (act) => act.name.toLowerCase() === formattedActivity.toLowerCase()
-      )
-    );
+  const excessParks = parksData.length - parksPerPage;
+  if (excessParks > 0) {
+    startCount -= excessParks;
+    parksData = parksData.slice(0, parksPerPage);
   }
 
   return {
@@ -33,8 +50,8 @@ export const fetchParks = async ({ queryKey, pageParam}) => {
       image: park.images && park.images[0] ? park.images[0].url : notFoundImage,
       url: park.url,
     })),
-    start: res.data.start,
-    total: res.data.total,
-    batch: parksBatchCount
+    start: startCount,
+    total: totalParks,
+    batch: formattedActivity ? totalFetched : 0,
   };
 };
